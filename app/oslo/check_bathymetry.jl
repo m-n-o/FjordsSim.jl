5bf1b14c8e3e124c329bf896a7cdd5d6cc7124a1
limash <yakubov.sha@gmail.com>
2024-03-17 21:50:26 +0100

    Fix nodes func call

--- app/oslo/check_bathymetry.jl
+++ app/oslo/check_bathymetry.jl
@@ -1,8 +1,7 @@
-using Oceananigans
-using GLMakie
 using FjordsSim
-
 using FjordsSim: ImmersedBoundaryGrid
+using GLMakie
+using Oceananigans
 
 fjords_setup = FjordsSetup(
     CPU(),
@@ -11,15 +10,16 @@ fjords_setup = FjordsSetup(
     (10.1, 11.1),
     (-500, 0),
     (4, 4, 4),
-    joinpath(homedir(), "fjos_data"),
+    joinpath(homedir(), "fjords_data"),
     "ETOPO_2022_v1_15s_N60E000_surface.nc",
     0,
     5,
 )
 
 grid = ImmersedBoundaryGrid(fjords_setup)
-λ, φ, z = nodes(grid, (grid.Lx, grid.Ly, grid.Lz))
+λ, φ, z = nodes(grid.underlying_grid, Center(), Center(), Center())
 
+h = grid.immersed_boundary.bottom_height
 land = interior(h) .>= 0
 interior(h)[land] .= NaN
 
