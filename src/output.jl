021c34ac410a18ab54de0bd769453c5d4968c9c1
Anfisa Berezina <abe@niva.no>
2024-10-17 12:34:16 +0200

    add time-z plot

--- src/output.jl
+++ src/output.jl
@@ -40,7 +40,9 @@ function record_surface_speed(
     end
 
     fig = Figure(size = (1000, 400))
-    ax = Axis(fig[1, 1])
+
+    title = @lift "Surface speed at " * prettytime(times[$iter]) 
+    ax = Axis(fig[1, 1], title = title)
     hm = heatmap!(ax, si, colorrange = colorrange, colormap = colormap)
     cb = Colorbar(fig[0, 1], hm, vertical = false, label = "Surface speed (ms⁻¹)")
     hidedecorations!(ax)
@@ -62,9 +64,10 @@ function record_surface_tracer(
          Ti[Ti .== 0] .= NaN
          Ti
     end
-     
+
+    title = @lift label * " at " * prettytime(times[$iter]) 
     fig = Figure(size = (1000, 400))
-    ax = Axis(fig[1, 1])
+    ax = Axis(fig[1, 1], title=title)
     hm = heatmap!(ax, Ti, colorrange = colorrange, colormap = colormap)
     cb = Colorbar(fig[0, 1], hm, vertical = false)
     hidedecorations!(ax)
@@ -88,7 +91,9 @@ function record_vertical_tracer(
     end
      
     fig = Figure(size = (1000, 400))
-    ax = Axis(fig[1, 1])
+
+    title = @lift label * " at " * prettytime(times[$iter]) 
+    ax = Axis(fig[1, 1], title = title)
     hm = heatmap!(ax, Ti, colorrange = colorrange, colormap = colormap)
     cb = Colorbar(fig[0, 1], hm, vertical = false)
     hidedecorations!(ax)
@@ -96,4 +101,70 @@ function record_vertical_tracer(
     CairoMakie.record(fig, joinpath(folder, "$(name).mp4"), 1:Nt, framerate = 8) do i
         iter[] = i
     end
+end
+
+
+
+function plot_ztime(
+    PHY, HET, POM, DOM, NUT, O₂, T, S, i, j, times, z, folder,
+    )
+
+    fig = Figure(size = (1500, 1000), fontsize = 20)
+
+    axis_kwargs = (
+        xlabel = "Time (days)",
+        ylabel = "z (m)",
+        xticks = (0:30:times[end]),
+        xtickformat = "{:.0f}" #   values -> ["$(value)kg" for value in values]     
+    )
+
+    axPHY = Axis(fig[1, 3]; title = "PHY, mmolN/m³", axis_kwargs...)
+    hmPHY = heatmap!(times / days, z, interior(PHY, i, j, :, :)', colormap = Reverse(:cubehelix)) #(:davos10))
+    Colorbar(fig[1, 4], hmPHY)
+
+    axHET = Axis(fig[2, 3]; title = "HET, mmolN/m³", axis_kwargs...)
+    hmHET = heatmap!(times / days, z, interior(HET, i, j, :, :)', colormap = Reverse(:afmhot))
+    Colorbar(fig[2, 4], hmHET)
+
+    axPOM = Axis(fig[3, 3]; title = "POM, mmolN/m³", axis_kwargs...)
+    hmPOM =
+        heatmap!(times / days, z, interior(POM, i, j, :, :)', colormap = Reverse(:greenbrownterrain)) #(:bilbao25))
+    hmPOM =
+        heatmap!(times / days, z, interior(POM, i, j, :, :)', colormap = Reverse(:greenbrownterrain)) #(:bilbao25))
+    Colorbar(fig[3, 4], hmPOM)
+
+    axDOM = Axis(fig[3, 1]; title = "DOM, mmolN/m³", axis_kwargs...)
+    hmDOM = heatmap!(times / days, z, interior(DOM, i, j, :, :)', colormap = Reverse(:CMRmap)) #(:devon10))
+    Colorbar(fig[3, 2], hmDOM)
+
+    axNUT = Axis(fig[1, 1]; title = "NUT, mmolN/m³", axis_kwargs...)
+    hmNUT = heatmap!(times / days, z, interior(NUT, i, j, :, :)', colormap = Reverse(:cherry))
+    hmNUT = heatmap!(times / days, z, interior(NUT, i, j, :, :)', colormap = Reverse(:cherry))
+    Colorbar(fig[1, 2], hmNUT)
+
+    axOXY = Axis(fig[2, 1]; title = "OXY, mmol/m³", axis_kwargs...)
+    hmOXY = heatmap!(times / days, z, interior(O₂, i, j, :, :)', colormap = :turbo)
+    hmOXY = heatmap!(times / days, z, interior(O₂, i, j, :, :)', colormap = :turbo)
+    Colorbar(fig[2, 2], hmOXY)
+
+    # axκ = Axis(fig[1, 5]; title = "κ  m³/s", axis_kwargs...)
+    # hmκ = heatmap!(times / days, z, interior(κ, 1, 1, :, :)', colormap = Reverse(:RdYlBu)) # :linear_grey_0_100_c0_n256)
+    # Colorbar(fig[1, 6], hmκ)
+
+    axT = Axis(fig[2, 5]; title = "T, oC", axis_kwargs...)
+    hmT = heatmap!(times / days, z, interior(T, i, j,  :, :)', colormap = Reverse(:RdYlBu))
+    Colorbar(fig[2, 6], hmT)
+
+    axS = Axis(fig[3, 5]; title = "S, psu", axis_kwargs...)
+    hmS = heatmap!(times / days, z, interior(S, i, j, :, :)', colormap = :viridis)
+    Colorbar(fig[3, 6], hmS)
+
+    # axPAR = Axis(fig[4, 1]; title = "PAR  μE⋅m-2⋅s-1", axis_kwargs...)
+    # hmPAR = heatmap!(times / days, z, interior(PAR, 1, 1, :, :)', colormap = :grayC100) # :linear_grey_0_100_c0_n256)
+    # Colorbar(fig[4, 2], hmPAR)
+
+    @info "VARIABLES Z-Time plots made"
+
+    save(joinpath(folder, "ztime.png"), fig)
+
 end
