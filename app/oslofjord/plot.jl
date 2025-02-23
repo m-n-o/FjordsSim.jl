527d44e3d737b9336c18409341057e40b7a40fb2
Anfisa Berezina <fisa4247@gmail.com>
2025-02-23 11:16:03 +0100

    update colormaps, ranges and lazy loading

--- app/oslofjord/plot.jl
+++ app/oslofjord/plot.jl
@@ -41,21 +41,27 @@ function record_variable(
 end
 
 folder = joinpath(homedir(), "FjordsSim_results", "oslofjord")
-filename = joinpath(folder, "snapshots")
-T = FieldTimeSeries("$filename.jld2", "T")
-S = FieldTimeSeries("$filename.jld2", "S")
-u = FieldTimeSeries("$filename.jld2", "u")
-v = FieldTimeSeries("$filename.jld2", "v")
+filename = joinpath(folder, "snapshots_year_oxydep")
+T = FieldTimeSeries("$filename.jld2", "T", backend=OnDisk())
+S = FieldTimeSeries("$filename.jld2", "S", backend=OnDisk())
+u = FieldTimeSeries("$filename.jld2", "u", backend=OnDisk())
+v = FieldTimeSeries("$filename.jld2", "v", backend=OnDisk())
+
+O₂ =  FieldTimeSeries("$filename.jld2", "O₂", backend=OnDisk())
+PHY =  FieldTimeSeries("$filename.jld2", "PHYT", backend=OnDisk())
 
 grid = jldopen("$filename.jld2")["grid"]
 Nz = grid["underlying_grid"]["Nz"]
 
-record_variable(T, "temperature surface", Nz, T.times, folder, (300, 700); colorrange = (-1, 20))
-record_variable(S, "salinity surface", Nz, S.times, folder, (300, 700); colorrange = (-1, 40))
-record_variable(u, "u velocity surface", Nz, u.times, folder, (300, 700); colorrange = (-5, 5))
-record_variable(v, "v velocity surface", Nz, v.times, folder, (300, 700); colorrange = (-5, 5))
+record_variable(T, "temperature surface", Nz, T.times, folder, (300, 700); colormap = Reverse(:RdYlBu), colorrange = (2, 5))
+record_variable(S, "salinity surface", Nz, S.times, folder, (300, 700); colormap = :viridis, colorrange = (20, 30))
+record_variable(u, "u velocity surface", Nz, u.times, folder, (300, 700); colorrange = (-1, 1))
+record_variable(v, "v velocity surface", Nz, v.times, folder, (300, 700); colorrange = (-1, 1))
 
-record_variable(T, "temperature bottom", 1, T.times, folder, (300, 700); colorrange = (-1, 20))
-record_variable(S, "salinity bottom", 1, S.times, folder, (300, 700); colorrange = (-1, 40))
-record_variable(u, "u velocity bottom", 1, u.times, folder, (300, 700); colorrange = (-5, 5))
-record_variable(v, "v velocity bottom", 1, v.times, folder, (300, 700); colorrange = (-5, 5))
+record_variable(O₂, "oxygen surface", Nz, O₂.times, folder, (300, 700); colormap = :turbo, colorrange = (2, 5))
+record_variable(PHY, "PHY surface", Nz, PHY.times, folder, (300, 700); colormap = Reverse(:cubehelix), colorrange = (20, 30))
+
+# record_variable(T, "temperature bottom", 1, T.times, folder, (300, 700); colorrange = (-1, 20))
+# record_variable(S, "salinity bottom", 1, S.times, folder, (300, 700); colorrange = (-1, 40))
+# record_variable(u, "u velocity bottom", 1, u.times, folder, (300, 700); colorrange = (-5, 5))
+# record_variable(v, "v velocity bottom", 1, v.times, folder, (300, 700); colorrange = (-5, 5))
